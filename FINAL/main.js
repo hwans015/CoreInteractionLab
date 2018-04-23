af16fcc7e255672230ae4fcc3272ec92612f1527
hwans015 <31640463+hwans015@users.noreply.github.com>
2018-04-23 10:04:16 -0400

    edit

--- FINAL/main.js
+++ FINAL/main.js
@@ -18,19 +18,38 @@ $(function () {
     contain: true,
     wrapAround: true,
   });
+
+   $('.image_carousel').flickity({
+    on: {
+      ready: function () {
+        console.log('Flickity is ready');
+      },
+      change: function (index) {
+        /*print index*/
+        /* This is where the numbers are being generated */
+        /* This is the call to our function that swaps out carousels */
+        oneAnimal(index);
+      }
+    },
+    cellAlign: 'left',
+    contain: true,
+    wrapAround: true,
+  });
+
   $('#image_carousel').flickity({
     cellAlign: 'left',
     contain: true,
     wrapAround: true,
   }); 
 
-});
-
-var $caption = $('.caption');
+  var $caption = $('.caption');
 // Flickity instance
-var flkty = $carousel.data('flickity');
+  var flkty = $carousel.data('flickity');
 
-$carousel.on( 'select.flickity', function() {
+  $carousel.on( 'select.flickity', function() {
   // set image caption using img's alt
   $caption.text( flkty.selectedElement.alt )
 });
+
+});
+
