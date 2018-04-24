ce62c7994e985b5db3904f17e1d61f369f172088
hwans015 <31640463+hwans015@users.noreply.github.com>
2018-04-24 16:06:38 -0400

    edit

--- FINAL/main.js
+++ FINAL/main.js
@@ -19,28 +19,37 @@ $(function () {
     wrapAround: true,
   });
 
-   $('.image_carousel').flickity({
-    on: {
-      ready: function () {
-        console.log('Flickity is ready');
-      },
-      change: function (index) {
-        /*print index*/
-        /* This is where the numbers are being generated */
-        /* This is the call to our function that swaps out carousels */
-        oneAnimal(index);
-      }
-    },
-    cellAlign: 'left',
-    contain: true,
-    wrapAround: true,
-  });
+  //  $('.image_carousel').flickity({
+  //   on: {
+  //     ready: function () {
+  //       console.log('Flickity is ready');
+  //     },
+  //     change: function (index) {
+  //       /*print index*/
+         // This is where the numbers are being generated 
+  //       /* This is the call to our function that swaps out carousels */
+  //       oneAnimal(index);
+  //     }
+  //   },
+  //   cellAlign: 'left',
+  //   contain: true,
+  //   wrapAround: true,
+  // });
 
   $('#image_carousel').flickity({
     cellAlign: 'left',
     contain: true,
     wrapAround: true,
   }); 
+
+  // jQuery(document).ready(function($){
+  //     $('.animaltop a').on('click',function(event){
+  //         event.preventDefault();
+  //         var indexElement = $(this).attr('href');
+  //         indexElement = indexElement.replace('#slide','');
+  //         $('.js-flickity').flickity( 'select', parseInt(indexElement)-1 );
+  //     });
+  // });
 
   var $caption = $('.caption');
 // Flickity instance
